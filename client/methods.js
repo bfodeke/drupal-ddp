06808283b28a92fa8efe192fb2df6ca8ac91b8e2
Bayo Fodeke <bfodeke@gmail.com>
2015-11-12 13:41:41 -0500

    Testing the usage of cookieMaster

--- client/methods.js
+++ client/methods.js
@@ -5,19 +5,33 @@ if (Meteor.isClient) {
         if (!err) {
           // Getting the upper level domain of the current site.
           var urlParts = location.hostname.split('.');
-          var domain = _.last(urlParts, 2).join('.');
+          // var domain = _.last(urlParts, 2).join('.');
+          var domain = _.last(urlParts, 1).join('.');
+          var cookieName, cookieValue;
 
           // Set a session cookie.
           var cookieParts = response.cookie.split(';');
 
+          console.log(cookieParts);
+          console.log('=== Meteor URL ===');
+          console.log(urlParts);
+
           // Loop through parts of the cookie to make it
           // work across subdomains.
           _.each(cookieParts, function(num, index){
+
             cookieSegment = num.split('=');
+
             if ($.trim(cookieSegment[0]) === 'domain') {
               // Set the domain equal to the tld.
               var cookieDomain = ' domain=' + domain;
               cookieParts[index] = cookieDomain;
+            }
+
+            // Set cookie name & value variables.
+            if (index === 0) {
+              cookieName = $.trim(cookieSegment[0]);
+              cookieValue = $.trim(cookieSegment[1]);
             }
 
             // If the 'HttpOnly' flag is set in the cookie,
@@ -25,11 +39,55 @@ if (Meteor.isClient) {
             if ($.trim(cookieSegment[0]) === 'HttpOnly') {
               cookieParts.splice(index, 1);
             }
+
           });
 
-          // Join all the cookie parts back together.
-          var sessionCookie = cookieParts.join(';');
-          document.cookie = sessionCookie;
+          // If on cordova, doc.cookie doesn't work. Use cookie master package
+          // instead.
+          if(Meteor.isCordova){
+            console.log('==== Cookie Name ====');
+            // alert('CookieName: ' + cookieName);
+
+            console.log('==== Cookie Value ====');
+            // alert('CookieVal: ' + cookieValue);
+
+            cookieMaster.setCookieValue('.' + domain, 'twoppl', 'allthevalues',
+              function() { alert('Set the twoppl cookie.');},
+              function() { alert('couldnt set new cookie');}
+            );
+
+            // cookieMaster.setCookieValue('.' + domain, cookieName, cookieValue,
+            // function() {
+            //   // alert('A cookie has been set');
+            // },
+            // function(error) {
+            //   alert('Error setting cookie: ' + error);
+            // });
+
+            var sessionCookie = cookieParts.join(';');
+            document.cookie = sessionCookie;
+
+            console.log('==== Cookie Master YES) ====');
+
+            cookieMaster.getCookieValue('.' + domain, 'twoppl', function(data) {
+              alert('Cookie Value: ' + data.cookieValue);
+            }, function(error) {
+              if (error) {
+                alert('Error getting cookie');
+              }
+            });
+
+          } else {
+            // Join all the cookie parts back together.
+            var sessionCookie = cookieParts.join(';');
+            document.cookie = sessionCookie;
+          }
+
+
+        }
+        else {
+          console.log('====== ERROROR ======');
+          console.log(err);
         }
       });
     }
